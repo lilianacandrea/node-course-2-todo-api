b7c486044f55eb45c1904b306948c681616f47dc
lilianacandrea <gogonicipici@gmail.com>
2018-07-03 13:54:32 +0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -74,6 +74,28 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+// validate if user exist with that email
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 // Mongoose middleware
 UserSchema.pre('save', function (next) {
   var user = this;
