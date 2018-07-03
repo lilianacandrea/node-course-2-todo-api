12704773375e0a314abac4560aefefcd85f267e7
lilianacandrea <gogonicipici@gmail.com>
2018-07-03 17:03:22 +0300

    Add test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -308,3 +308,27 @@ describe('POST /users/login', (done) => {
         });
     });
 });
+
+describe('DELETE /users/me/login', () => {
+  it('should remove auth token on logout', (done) => {
+    // delete request /users/me/token
+    // set x-auth equal to token
+    // 200
+    // find user, verify that tokens array has length of zero
+
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+      User.findById(users[0]._id).then((user) => {
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+});
